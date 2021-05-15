c23672433e0871b0879028fed512de52a6c1745b
antoniojh10 <antoniohernandez95@gmail.com>
2021-05-15 19:20:00 -0400

    style: :art: python formatting

--- 1-pycardio/triangle_area.py
+++ 1-pycardio/triangle_area.py
@@ -1,15 +1,15 @@
 def calc_triangle_area(base, height):
-  return (base * height) / 2.0
+    return (base * height) / 2.0
+
 
 def run():
-  triangle_base = float(input('Ingresa el valor de la base del triángulo: '))
-  triangle_height = float(input('Ingresa el valor de la altura del triángulo: '))
+    triangle_base = float(input("Ingresa el valor de la base del triángulo: "))
+    triangle_height = float(input("Ingresa el valor de la altura del triángulo: "))
 
-  triangle_area = calc_triangle_area(triangle_base, triangle_height)
+    triangle_area = calc_triangle_area(triangle_base, triangle_height)
 
-  print(f"El área del triángulo es de: {triangle_area}")
+    print(f"El área del triángulo es de: {triangle_area}")
 
 
-if __name__ == '__main__':
-  run()
-
+if __name__ == "__main__":
+    run()
