b9eba55adf65a38a2e8ef91dc5aca7bd8dcd5b3e
antoniojh10 <antoniohernandez95@gmail.com>
2021-05-15 21:58:53 -0400

    fix: :bug: Error handling in Python

--- 1-pycardio/triangle_area.py
+++ 1-pycardio/triangle_area.py
@@ -1,10 +1,26 @@
 def calc_triangle_area(base, height):
-    return (base * height) / 2.0
+    area = (base * height) / 2.0
+    return round(area, 2)
 
 
 def run():
-    triangle_base = float(input("Ingresa el valor de la base del triángulo: "))
-    triangle_height = float(input("Ingresa el valor de la altura del triángulo: "))
+    status = "base"
+
+    while status == "base":
+        try:
+            triangle_base = float(input("Ingresa el valor de la base del triángulo: "))
+        except ValueError:
+            print("Ingresa un número")
+        else:
+            status = "altura"
+
+    while status == "altura":
+        try:
+            triangle_height = float(input("Ingresa el valor de la altura del triángulo: "))
+        except ValueError:
+            print("Ingresa un número")
+        else:
+            status = "area"
 
     triangle_area = calc_triangle_area(triangle_base, triangle_height)
 
